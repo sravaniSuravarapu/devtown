4e90a9c8ae1f3e1d8953b3a0a77ae55debcd9ecd
sravanisuravapu3@gmail.com <sravanisuravapu3@gmail.com>
2023-12-23 17:47:41 +0530

    any commit message

---
+++ src/constants/index.js
@@ -0,0 +1,144 @@
+import {
+  spfOne,
+  spfTwo,
+  spfThree,
+  spfFour,
+  bestSellerOne,
+  bestSellerTwo,
+  bestSellerThree,
+  bestSellerFour,
+  newArrOne,
+  newArrTwo,
+  newArrThree,
+  newArrFour,
+} from "../assets/images/index";
+
+// =================== NavBarList Start here ====================
+export const navBarList = [
+  {
+    _id: 1001,
+    title: "Home",
+    link: "/",
+  },
+  {
+    _id: 1002,
+    title: "Shop",
+    link: "/shop",
+  },
+  {
+    _id: 1003,
+    title: "About",
+    link: "/about",
+  },
+  {
+    _id: 1004,
+    title: "Contact",
+    link: "contact",
+  },
+  {
+    _id: 1005,
+    title: "Journal",
+    link: "/journal",
+  },
+];
+// =================== NavBarList End here ======================
+// =================== Special Offer data Start here ============
+export const SplOfferData = [
+  {
+    _id: "201",
+    img: spfOne,
+    productName: "vivos1",
+    price: "3500.00",
+    color: "Blank and White",
+    badge: true,
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis.",
+  },
+  {
+    _id: "202",
+    img: newArrFour,
+    productName: "realme ",
+    price: "180.00",
+    color: "Gray",
+    badge: true,
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis.",
+  },
+  {
+    _id: "203",
+    img: spfThree,
+    productName: "samsung",
+    price: "250.00",
+    color: "Mixed",
+    badge: true,
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis.",
+  },
+  {
+    _id: "204",
+    img: spfFour,
+    productName: "oneplus  ",
+    price: "220.00",
+    color: "Black",
+    badge: true,
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis.",
+  },
+];
+// =================== Special Offer data End here ==============
+
+// =================== PaginationItems Start here ===============
+
+export const paginationItems = [
+  {
+    _id: 1001,
+    img: spfOne,
+    productName: "vivos1",
+    price: "35.00",
+    color: "Blank and White",
+    badge: true,
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis.",
+  },
+  {
+    _id: 1002,
+    img: spfTwo,
+    productName: "realme",
+    price: "180.00",
+    color: "Gray",
+    badge: true,
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis.",
+  },
+  {
+    _id: 1003,
+    img: spfThree,
+    productName: "poco",
+    price: "25.00",
+    color: "Mixed",
+    badge: true,
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis.",
+  },
+  {
+    _id: 1004,
+    img: spfFour,
+    productName: "iphone",
+    price: "220.00",
+    color: "Black",
+    badge: true,
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis.",
+  },
+  {
+    _id: 1005,
+    img: bestSellerOne,
+    productName: "realme",
+    price: "35.00",
+    color: "Blank and White",
+    badge: true,
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis.",
+  },
+  {
+    _id: 1006,
+    img: bestSellerTwo,
+    productName: "vivot1x",
+    price: "180.00",
+    color: "Gray",
+    badge: false,
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis.",
+  }
+];
+// =================== PaginationItems End here =================
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Banner from "../../components/Banner/Banner";
+import BannerBottom from "../../components/Banner/BannerBottom";
+import BestSellers from "../../components/home/BestSellers/BestSellers";
+import NewArrivals from "../../components/home/NewArrivals/NewArrivals";
+import SpecialOffers from "../../components/home/SpecialOffers/SpecialOffers";
+
+const Home = () => {
+  return (
+    <div className="w-full mx-auto">
+      <Banner />
+      <BannerBottom />
+      <div className="max-w-container mx-auto px-4">
+        <NewArrivals />
+        <BestSellers />
+        <SpecialOffers />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
